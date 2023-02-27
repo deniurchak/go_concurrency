ab4ad9fba8d8c7fa5deea227b3431613b88a1ada
deniurchak <swdenis@mail.ru>
2023-02-27 20:44:33 +0100

    Add example with many routines

--- goroutines/many_routines/main.go
+++ goroutines/many_routines/main.go
@@ -7,17 +7,19 @@ import (
 func main() {
 	// create a notification channel
 	ch := make(chan struct{})
+	var counter = 0
 	// run the sample go routine
-	go sample(ch)
-	go sample(ch)
-	go sample(ch)
+	go sample(&counter, ch)
+	go sample(&counter, ch)
+	go sample(&counter, ch)
 	<-ch
 	<-ch
 	<-ch
 }
 
-func sample(ch chan struct{}) {
-	fmt.Println("new routine")
+func sample(counter *int, ch chan struct{}) {
+	*counter++
+	fmt.Println(*counter)
 	// send a notification to the channel
 	ch <- struct{}{}
 }
